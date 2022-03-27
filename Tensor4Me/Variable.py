16899018c326b0f808ba8e58650b36d46f2ce85f
sina-Kim <sinabero3271@gmail.com>
2022-03-27 16:00:54 +0900

    auto backward 구현 중...

--- Tensor4Me/Variable.py
+++ Tensor4Me/Variable.py
@@ -3,3 +3,14 @@ class Variable(object):
         # Do not implement until actual needs!
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
